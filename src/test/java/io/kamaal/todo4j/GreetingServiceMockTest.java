92e94b01f6710d3bccc7c5020ddc7703ce700e5b
Kamaal Farah <kamaal.f1@gmail.com>
2025-04-13 20:55:11 +0200

    Mocking service in test

---
+++ src/test/java/io/kamaal/todo4j/GreetingServiceMockTest.java
@@ -0,0 +1,34 @@
+package io.kamaal.todo4j;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.hamcrest.Matchers.containsString;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest(GreetingController.class)
+public class GreetingServiceMockTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockitoBean
+    private GreetingService service;
+
+    @Test
+    void greetingShouldReturnMessageFromService() throws Exception {
+        when(service.greet("World"))
+                .thenReturn("Hello, Mock");
+
+        mockMvc.perform(get("/greeting"))
+                .andExpect(status().isOk())
+                .andExpect(content().string(containsString("Hello, Mock")));
+    }
+
+}
